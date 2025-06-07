11687f753ff1a2f4faa273697cdb5ea951178492
viniairam7 <viniairam2@gmail.com>
2025-06-07 16:08:42 -0300

    Update VendaTestes.java

--- test/VendaTestes.java
+++ test/VendaTestes.java
@@ -4,24 +4,27 @@ import lojatgid.ItemVenda;
 import lojatgid.Produto;
 import lojatgid.Usuario;
 import lojatgid.Venda;
+import lojatgid.exceptions.CpfException;
+import lojatgid.exceptions.CpfExceptionII;
+import lojatgid.exceptions.TelefoneException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VendaTestes {
 
     @Test
-    public void testTotalVenda() {
+    public void testTotalVenda() throws CpfException, TelefoneException, CpfExceptionII {
         Produto produto1 = new Produto("Produto A", 100.0, 10);
         Produto produto2 = new Produto("Produto B", 50.0, 20);
 
-        ItemVenda item1 = new ItemVenda(produto1.getNome(), produto1.getPreco(), produto1.getQuantidade(), 2);  // 2 unidades
-        ItemVenda item2 = new ItemVenda(produto2.getNome(), produto2.getPreco(), produto2.getQuantidade(), 1);  // 1 unidade
+        ItemVenda item1 = new ItemVenda(produto1.getNome(), produto1.getPrecoRS(), produto1.getQuantidade(), 2);  // 2 unidades
+        ItemVenda item2 = new ItemVenda(produto2.getNome(), produto2.getPrecoRS(), produto2.getQuantidade(), 1);  // 1 unidade
 
         List<ItemVenda> itensVenda = new ArrayList<>();
         itensVenda.add(item1);
         itensVenda.add(item2);
 
-        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "Rua Alves de Souza, 22 - Osasco");
+        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "11982288228", "Rua Alves de Souza, 22 - Osasco");
 
         Venda venda = new Venda(usuario, itensVenda);
 
