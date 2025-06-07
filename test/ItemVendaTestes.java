da508c9320c500fdc220bc2f96f90de90155ffc4
viniairam7 <viniairam2@gmail.com>
2025-06-07 14:33:01 -0300

    feat: adicionar Testes Unitários a classe ItemVenda
    Foram criados testes unitários para a classe ItemVenda, verificando o produto e valor.

---
+++ test/ItemVendaTestes.java
@@ -0,0 +1,15 @@
+import lojatgid.ItemVenda;
+import lojatgid.Produto;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemVendaTestes {
+
+    @Test
+    public void testTotalItemVenda() {
+        Produto produto = new Produto("Produto A", 100.0, 10);
+        ItemVenda item = new ItemVenda(produto.getNome(), produto.getPreco(), produto.getQuantidade(), 2);  // 2 unidades
+
+        assertEquals(200.0, item.getTotal(), "O total do item de venda deve ser 200.0");
+    }
+}
