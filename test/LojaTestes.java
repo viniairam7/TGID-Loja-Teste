7578cfac144eb1baa6bd991ad2a9db0fa9587b8f
viniairam7 <viniairam2@gmail.com>
2025-06-07 14:19:04 -0300

    feat: adicionar teste unitário a classe Loja
    Testes unitários para a classe Loja, verificando adição de produtos e usuários.

--- test/LojaTestes.java
+++ test/LojaTestes.java
@@ -1,43 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+import java.util.ArrayList;
+import java.util.List;
+import lojatgid.ItemVenda;
+import lojatgid.Loja;
+import lojatgid.Produto;
+import lojatgid.Usuario;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author Airam
- */
 public class LojaTestes {
-    
-    public LojaTestes() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+
+    @Test
+    public void testAdicionarProduto() {
+        Loja loja = new Loja();
+        Produto produto = new Produto("Produto A", 100.0, 10);
+        loja.adicionarProduto(produto);
+
+        assertEquals(1, loja.produtos.size(), "A loja deve ter 1 produto");
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testCadastrarUsuario() {
+        Loja loja = new Loja();
+        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "Rua Alves de Souza, 22 - Osasco");
+        loja.cadastrarUsuario(usuario);
+
+        assertEquals(1, loja.usuarios.size(), "A loja deve ter 1 usuário cadastrado");
+    }
+
+    @Test
+    public void testRealizarVenda() {
+        Loja loja = new Loja();
+        Produto produto = new Produto("Produto A", 100.0, 10);
+        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "Rua Alves de Souza, 22 - Osasco");
+
+        loja.adicionarProduto(produto);
+        loja.cadastrarUsuario(usuario);
+
+        ItemVenda itemVenda = new ItemVenda(produto.getNome(), produto.getPreco(), produto.getQuantidade(), 2);
+        List<ItemVenda> itensVenda = new ArrayList<>();
+        itensVenda.add(itemVenda);
+
+        loja.realizarVenda(usuario, itensVenda);
+
+        assertEquals(8, produto.getQuantidade(), "O estoque do produto deve ser atualizado após a venda");
+    }
 }
