f2a2d23956a14028c4a6983030f4cd6f2349b906
viniairam7 <viniairam2@gmail.com>
2025-06-07 12:39:45 -0300

    feat: Adicionar variáveis e métodos
    Foram adicionados os atributos, construtores e métodos da classe Venda.

--- lojatgid/Venda.java
+++ lojatgid/Venda.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lojatgid;
 
-/**
- *
- * @author Airam
- */
+import java.util.List;
+
 public class Venda {
+    private Usuario usuario;  
+    private List<ItemVenda> itensVenda;  
+    private double total;
+
     
+    public Venda(Usuario usuario, List<ItemVenda> itensVenda) {
+        this.usuario = usuario;
+        this.itensVenda = itensVenda;
+        calcularTotal();
+    }
+
+    
+    private void calcularTotal() {
+        total = 0;
+        for (ItemVenda item : itensVenda) {
+            total += item.getTotal(); // para calcular o total da venda, somando o valor de cada ItemVenda //
+        }
+    }
+
+ 
+    public double getTotal() {
+        return total;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public List<ItemVenda> getItensVenda() {
+        return itensVenda;
+    }
+
+    @Override
+    public String toString() {
+        return "Venda{" + "usuario=" + usuario + ", itensVenda=" + itensVenda + ", total=" + total + '}';
+    }
 }
