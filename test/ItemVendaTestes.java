d466d8b557d84443c819c3d0be0aae2d19cf82e4
viniairam7 <viniairam2@gmail.com>
2025-06-07 16:08:33 -0300

    Update ItemVendaTestes.java

--- test/ItemVendaTestes.java
+++ test/ItemVendaTestes.java
@@ -8,7 +8,7 @@ public class ItemVendaTestes {
     @Test
     public void testTotalItemVenda() {
         Produto produto = new Produto("Produto A", 100.0, 10);
-        ItemVenda item = new ItemVenda(produto.getNome(), produto.getPreco(), produto.getQuantidade(), 2);  // 2 unidades
+        ItemVenda item = new ItemVenda(produto.getNome(), produto.getPrecoRS(), produto.getQuantidade(), 2);  // 2 unidades
 
         assertEquals(200.0, item.getTotal(), "O total do item de venda deve ser 200.0");
     }
