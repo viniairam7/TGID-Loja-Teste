ccb3b110fc84b862671650aeeb6f532cdcd31cf1
viniairam7 <viniairam2@gmail.com>
2025-06-07 13:45:33 -0300

    feat: Adicionar testes unitários para a classe Usuario
    Foram criados testes unitários para a classe Usuario, garantindo que os métodos getters (como getNome(), getCpf(), getEmail(), e getEndereco()) funcionem corretamente, retornando os valores esperados. Além disso, o método toString() foi testado para verificar se a string gerada está correta e se inclui todos os atributos da classe Usuario.

---
+++ test/UsuarioTestes.java
@@ -0,0 +1,28 @@
+import lojatgid.Usuario;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UsuarioTestes {
+
+    @Test
+    public void testGetters() {
+        
+        Usuario usuario = new Usuario("João", "123.456.789-00", "joao@email.com", "Rua Alves de Souza, 22 - Osasco");
+
+        
+        assertEquals("Vinicius", usuario.getNome(), "O nome do usuário deve ser Vinicius");
+        assertEquals("123.456.789-00", usuario.getCpf(), "O CPF do usuário deve ser 123.456.789-00");
+        assertEquals("vini@email.com", usuario.getEmail(), "O email do usuário deve ser vini@email.com");
+        assertEquals("Rua Alves de Souza, 22 - Osasco", usuario.getEndereco(), "O endereço do usuário deve ser Rua Alves de Souza, 22 - Osasco");
+    }
+
+    @Test
+    public void testToString() {
+        // Criando um usuário
+        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "Rua Alves de Souza, 22 - Osasco");
+
+        // Verificando se o método toString retorna a string correta
+        String expected = "Usuario:" + "nome='Vinicius', cpf='123.456.789-00', email='vini@email.com', residente='Rua Alves de Souza, 22 - Osasco'";
+        assertEquals(expected, usuario.toString(), "O método toString deve retornar a representação correta do usuário");
+    }
+}
