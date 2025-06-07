7676d99591dd0fff76fbdd689892e0f6ff966dc0
viniairam7 <viniairam2@gmail.com>
2025-06-07 16:08:36 -0300

    Update LojaTestes.java

--- test/LojaTestes.java
+++ test/LojaTestes.java
@@ -4,14 +4,19 @@ import lojatgid.ItemVenda;
 import lojatgid.Loja;
 import lojatgid.Produto;
 import lojatgid.Usuario;
+import lojatgid.exceptions.CpfException;
+import lojatgid.exceptions.CpfExceptionII;
+import lojatgid.exceptions.EstoqueException;
+import lojatgid.exceptions.ProdutoException;
+import lojatgid.exceptions.TelefoneException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LojaTestes {
 
     @Test
-    public void testAdicionarProduto() {
-        Loja loja = new Loja();
+    public void testAdicionarProduto() throws ProdutoException {
+        Loja loja = new Loja("Loja TGID");
         Produto produto = new Produto("Produto A", 100.0, 10);
         loja.adicionarProduto(produto);
 
@@ -19,24 +24,24 @@ public class LojaTestes {
     }
 
     @Test
-    public void testCadastrarUsuario() {
-        Loja loja = new Loja();
-        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "Rua Alves de Souza, 22 - Osasco");
+    public void testCadastrarUsuario() throws CpfException, TelefoneException, CpfExceptionII {
+        Loja loja = new Loja("Loja TGID");
+        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "11982288228","Rua Alves de Souza, 22 - Osasco");
         loja.cadastrarUsuario(usuario);
 
         assertEquals(1, loja.usuarios.size(), "A loja deve ter 1 usuário cadastrado");
     }
 
     @Test
-    public void testRealizarVenda() {
-        Loja loja = new Loja();
+    public void testRealizarVenda() throws CpfException, TelefoneException, CpfExceptionII, ProdutoException, EstoqueException {
+        Loja loja = new Loja("Loja TGID");
         Produto produto = new Produto("Produto A", 100.0, 10);
-        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com", "Rua Alves de Souza, 22 - Osasco");
+        Usuario usuario = new Usuario("Vinicius", "123.456.789-00", "vini@email.com","11982288228", "Rua Alves de Souza, 22 - Osasco");
 
         loja.adicionarProduto(produto);
         loja.cadastrarUsuario(usuario);
 
-        ItemVenda itemVenda = new ItemVenda(produto.getNome(), produto.getPreco(), produto.getQuantidade(), 2);
+        ItemVenda itemVenda = new ItemVenda(produto.getNome(), produto.getPrecoRS(), produto.getQuantidade(), 2);
         List<ItemVenda> itensVenda = new ArrayList<>();
         itensVenda.add(itemVenda);
 
