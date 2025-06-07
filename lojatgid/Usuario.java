6a67cfad682b4335c29c6afa36ae82148339150f
viniairam7 <viniairam2@gmail.com>
2025-06-07 12:00:46 -0300

    Update Usuario.java

--- lojatgid/Usuario.java
+++ lojatgid/Usuario.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lojatgid;
 
-/**
- *
- * @author Airam
- */
 public class Usuario {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String endereco;
+
+    public Usuario(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
     
+    public String getEndereco() {
+        return endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario:" + "nome='" + nome + '\'' + ", cpf='" + cpf + '\'' + ", email='" + email + '\'' + ", residente='" + endereco + '\'' + '}';
+    }
 }
