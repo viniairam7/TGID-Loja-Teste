83f983fe911ae76228064da4b3e11ada874075f6
viniairam7 <viniairam2@gmail.com>
2025-06-07 13:15:25 -0300

    feat: Adicionar Listas, atributos e métodos
    Implementada a busca de produtos pelo nome e a verificação de estoque antes de realizar uma venda, garantindo que o estoque seja suficiente para a quantidade solicitada. Além disso, a lógica de venda agora diminui o estoque diretamente através dos métodos herdados de Produto na classe ItemVenda.

--- lojatgid/Loja.java
+++ lojatgid/Loja.java
@@ -1,10 +1,73 @@
 package lojatgid;
-/**@author Airam
- */
-public class Loja {
 
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loja {
+    public List<Produto> produtos;  
+    private List<Usuario> usuarios;  
+    private List<Venda> vendas;      
+
+    
+    public Loja() {
+        this.produtos = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+        this.vendas = new ArrayList<>();
+    }
+
+    
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    
+    public void cadastrarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    
+    public Produto buscarProdutoPorNome(String nomeProduto) {
+        for (Produto produto : produtos) {
+            if (produto.getNome().equals(nomeProduto)) {  
+                return produto;
+            }
+        }
+        return null;  
+    }
+
+    
+    public void realizarVenda(Usuario usuario, List<ItemVenda> itensVenda) {
+        
+        for (ItemVenda item : itensVenda) {
+            if (item.getQuantidade() > item.getQuantidade()) {
+                System.out.println("Estoque insuficiente para o produto: " + item.getNome());
+                return;  
+            }
+        }
+
+        
+        for (ItemVenda item : itensVenda) {
+            item.diminuirEstoque(item.getQuantidade());  
+        }
+
+        
+        Venda venda = new Venda(usuario, itensVenda);
+        vendas.add(venda);  
+
         
     }
+
     
+    public void exibirResumoVendas() {
+        for (Venda venda : vendas) {
+            System.out.println(venda);
+        }
+    }
+
+   
+    public void exibirEstoque() {
+        for (Produto produto : produtos) {
+            System.out.println(produto);
+        }
+    }
 }
