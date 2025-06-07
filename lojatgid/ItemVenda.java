4a81f40dd4d780e3b50d17c65b92473708d14f8f
viniairam7 <viniairam2@gmail.com>
2025-06-07 12:26:21 -0300

    feat: Adicionar herança e métodos
    Foi adicionado a classe ItemVenda o campo quantidadeVendida, que representa a quantidade do produto vendida, além do método getTotal() para calcular o valor da venda e a sobrescrita do método diminuirEstoque() para ajustar o estoque após a venda.

--- lojatgid/ItemVenda.java
+++ lojatgid/ItemVenda.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lojatgid;
 
-/**
- *
- * @author Airam
- */
-public class ItemVenda {
+public class ItemVenda extends Produto {
+    private int quantidadeVendida;
+
+    public ItemVenda(String objeto, double preco, int quantidadeEstoque, int quantidadeVendida) {
+        // aciona o construtor da classe Produto // 
+        super(objeto, preco, quantidadeEstoque);
+        this.quantidadeVendida = quantidadeVendida;
+    }
+
+    public int getQuantidadeVendida() {
+        return quantidadeVendida;
+    }
+
+    public void setQuantidadeVendida(int quantidadeVendida) {
+        this.quantidadeVendida = quantidadeVendida;
+    }
+
+    @Override
+    public void diminuirEstoque(int quantidade) {
+        // pode ser verificado a quantidade de produto //
+        if (super.getQuantidade() >= quantidade) {
+            super.diminuirEstoque(quantidade);
+        }
+    }
+
     
+    public double getTotal() {
+        return getPreco() * quantidadeVendida;
+    }
+       // calcula o total do item em relação ao preço e quantidade //
+    @Override
+    public String toString() {
+        return "ItemVenda{" +
+                "produto=" + getObjeto() + 
+                ", quantidadeVendida=" + quantidadeVendida + 
+                ", total=" + getTotal() + '}';
+    }
 }
